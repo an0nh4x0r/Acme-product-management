a138a7a8b80947f5a051bb89c253f1de6b242c33
Swapnil Singh <swappp7singh@gmail.com>
2017-01-31 02:44:16 +0530

    implemented *ngIf and *ngFor directives in the product-list component currently using hardcoded data but soon will pull this data from the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Acme product management';
+  pageTitle: string = 'Acme product management';
 }
